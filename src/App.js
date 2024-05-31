d100f47ebd4664766786fae23f39410e90753797
acroak <acroak@users.noreply.github.com>
2024-05-31 16:35:15 -0400

    convert the class component version of the app into a functional component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,43 @@
-import { Component } from 'react';
-
+import { useState, useEffect } from 'react';
 import CardList from './components/card-list/card-list.component.jsx';
 import SearchBox from './components/search-box/search-box.component.jsx';
 
 import './App.css';
 
+const App = () => {
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-        profiles: [],
-        searchField: '',
-    };
-    console.log('constructor');
-  }
+    const [searchField, setSearchField] = useState('');//[value, setValue], pass the intial value
+    const[profiles, setProfiles] = useState([]);
 
-  
-  componentDidMount(){
-    //API request for array of user pofiles
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => 
-            //convert response body into JSON
-            response.json()
-        )
-        .then((users) => 
-            // set state of profile array
-            this.setState(() => {
-                return { profiles: users }
-            },
-            // callback for when the set state is finished to make sure state has the correct value
-            () => {
-                console.log(this.state)
-            }
-        )
-    );
+    //filtered profiles will always be based off of initital profiles array
+    const[filteredProfiles, setFilteredProfiles] = (useState(profiles));
 
-  }
+    const onSearchChange = (event) => {
+        const searchFieldString = event.target.value.toLocaleLowerCase();
+        setSearchField(searchFieldString);
+    }
 
-  /* filter out elements of the array based on input data
-    * filter() creates a new array based on the returned includes boolean
-    * if this profile name includes this searched string, keep it. else get rid of it.
-    * searches the untouched/un-edited original list of profiles to generate the displayed list down in render()
-  */
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    //change the state
-        this.setState(() => {
-            return{ searchField };
+    // fetch our list of profiles from API
+    useEffect(()=> {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        //convert response body into JSON
+        .then((response) => response.json())
+         // set state of profiles array
+        .then((users) => setProfiles(users));
+    },[]);
+
+    //Only filter profiles if the searchFieldString or profiles array has changed
+    useEffect(() => {
+        const newFilteredProfiles = profiles.filter((profile) => {
+            //includes() returns a boolean
+            return profile.name.toLocaleLowerCase().includes(searchField);
         });
-    };
 
-  render() {
-    // destructuring, shorten the length of variable names
-    const { profiles, searchField } = this.state;
-    const { onSearchChange } = this;
+        setFilteredProfiles(newFilteredProfiles);
 
-    const filteredProfiles = profiles.filter((profile) => {
-        //includes() returns a boolean
-        return profile.name.toLocaleLowerCase().includes(searchField);
-    });
+    },[searchField, profiles] )
 
+    
     return(
         <div className='App'>
             <h1 className='app-title'>Rolodex</h1>
@@ -69,11 +46,12 @@ class App extends Component {
                 placeholder = 'search profiles'
                 onChangeHandler = {onSearchChange}  
             />
+
             <CardList profiles = {filteredProfiles}/>
 
         </div>
-    );
-    }
+    )
 }
+
 
 export default App;
