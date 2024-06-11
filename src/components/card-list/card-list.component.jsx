92bc945a1d7690b07e9313acfc9cd43b371cd6f6
acroak <acroak@users.noreply.github.com>
2024-06-10 21:52:55 -0400

    finish conversion to functional components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,24 +1,17 @@
-import { Component } from "react";
-
 import Card from "../card/card.component";
 import "./card-list.styles.css";
 
-class CardList extends Component {
-    render() {
-        const { profiles } = this.props;
-        
-        return (
-            <div className='card-list'>
-            
-                { profiles.map( (profile) => {
+const CardList = ({profiles}) => {
+    return(
+       <div className='card-list'>
+            { profiles.map( (profile) => {
                 
-                    return(
-                       <Card profile = {profile} />
-                    );
-                })}
-            </div>
-        )
-    }
+                return(
+                    <Card profile = { profile } />
+                );
+            })}
+        </div>
+    )
 }
 
 export default CardList;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,24 +1,23 @@
-import { Component } from "react";
 import './card.styles.css';
 
-class Card extends Component {
+const Card = ( {profile} ) => {
 
-    render() {
-        const {name, email, id} = this.props.profile;
+    
+    const {name, email, id} = profile;
 
-        return (
-            <div className="card-container" key={id}>
-                <img 
-                    className="card-img"
-                    alt={ `profile img of ${name}`} 
-                    src={`https://robohash.org/${id}?set=set5&size=180x180`} 
-                />
-                <h2>{ name }</h2>
-                <p>{ email }</p>
-            </div>
+    return (
+        <div className="card-container" key={id}>
+            <img 
+                className="card-img"
+                alt={ `profile img of ${name}`} 
+                src={`https://robohash.org/${id}?set=set5&size=180x180`} 
+            />
+            <h2>{ name }</h2>
+            <p>{ email }</p>
+        </div>
 
-        );
-    }
+    );
+    
 }
 
 export default Card;
